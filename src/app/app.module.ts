414d9f5d16254e355a87745ee0fb8a7a54952037
suphawit19 <suphawit1942@gmail.com>
2022-01-23 17:25:26 +0700

    -

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-// import { firebaseConfig } from 'src/environments/firebase.config';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+import { firebaseConfig } from 'src/environments/firebase.config';
+
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-import { AngularFirestoreCollection, AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireModule } from '@angular/fire/compat';
-import { firebaseConfig } from 'src/environments/environment';
-
-
-
 
 @NgModule({
   declarations: [
